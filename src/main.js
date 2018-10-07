9f93eeca2f58d43dde3d37c6866b50d84802e9f5
Md. Abdul Kaium <abdulkaium1568@gmail.com>
2018-10-07 17:41:59 +0600

    React Hard Loading Done

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
 require("babel-runtime/regenerator");
+require("react-hot-loader/patch");
+require("babel-register");
 require("webpack-hot-middleware/client?reload=true");
 require("./main.css");
 require("./index.html");
+require("./app.js");
+// alert("Loaded");
 
-alert("Loaded");
-
-var a = async args => {
-    const {
-        a,
-        b
-    } = args;
-    await console.log("Hello From The babel!");
-    console.log("Done");
-}
-debugger
-a({
-    a: 1,
-    b: 4
-});
+// var a = async args => {
+//     const {
+//         a,
+//         b
+//     } = args;
+//     await console.log("Hello From The babel!");
+//     console.log("Done");
+// }
+// debugger
+// a({
+//     a: 1,
+//     b: 4
+// });
